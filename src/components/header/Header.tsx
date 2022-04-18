d975adc4e4399c50e76e24070ec3012a1616c45b
leomunizq <leonardo.mdq@hotmail.com>
2022-04-18 17:02:30 +0200

    add CSS style.

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,7 +2,15 @@ import React from 'react'
 import './header.scss'
 
 const Header = () => {
-  return <div>Header</div>
+  return (
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Leonardo Muniz</h1>
+        <h5 className="text-light">Front end Developer</h5>
+      </div>
+    </header>
+  )
 }
 
 export default Header
