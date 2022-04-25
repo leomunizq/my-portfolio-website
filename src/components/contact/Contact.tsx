b4791c9f08201d7757f6b937c99688114655ed2b
leomunizq <leonardo.mdq@hotmail.com>
2022-04-25 17:29:31 +0200

    add Contact and Footer Components.

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,8 +1,83 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import './contact.scss'
+import { MdOutlineEmail } from 'react-icons/md'
+import { RiMessengerLine } from 'react-icons/ri'
+import { BsWhatsapp } from 'react-icons/bs'
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
-  return <section id="contact">Contact</section>
+  const form: any = useRef()
+
+  const sendEmail = (e: any) => {
+    e.preventDefault()
+
+    emailjs.sendForm(
+      'service_ycb9asa',
+      'template_929zk5e',
+      form.current,
+      'TY9tm9pQC8iBrdarF'
+    )
+    e.target.reset()
+  }
+
+  return (
+    <section id="contact">
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className="contact__option">
+            <MdOutlineEmail className="contact__option-icon" />
+            <h4>Email</h4>
+            <h5>leonardo.mdq@hotmail.com</h5>
+            <a href="mailto:leonardo.mdq@hotmail.com" target="_blank">
+              Send a Message
+            </a>
+          </article>
+          <article className="contact__option">
+            <RiMessengerLine className="contact__option-icon" />
+            <h4>Messenger</h4>
+            <h5>Leonardo M.</h5>
+            <a href="https://m.me/leomuniz.q/" target="_blank">
+              Send a Message
+            </a>
+          </article>
+          <article className="contact__option">
+            <BsWhatsapp className="contact__option-icon" />
+            <h4>Whatsapp</h4>
+            <h5>+39 331 7309368</h5>
+            <a href="https://wa.me/393317309368" target="_blank">
+              Send a Message
+            </a>
+          </article>
+        </div>
+        {/* END OF CONTACT */}
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="name"
+            placeholder="Your Full Name"
+            required
+          />
+          <input
+            type="email"
+            name="email"
+            placeholder="Your email address"
+            required
+          />
+          <textarea
+            name="message"
+            rows={7}
+            placeholder="Your message"
+            required
+          ></textarea>
+          <button type="submit" className="btn btn-primary">
+            Send a Message
+          </button>
+        </form>
+      </div>
+    </section>
+  )
 }
 
 export default Contact
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,8 +1,55 @@
 import React from 'react'
 import './footer.scss'
+import { BsLinkedin } from 'react-icons/bs'
+import { BsGithub } from 'react-icons/bs'
+import { CgFigma } from 'react-icons/cg'
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer>
+      <a href="#" className="footer__logo">
+        Leonardo Muniz
+      </a>
+      <ul className="permalinks">
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#About">About</a>
+        </li>
+        <li>
+          <a href="#Experience">Experience</a>
+        </li>
+        <li>
+          <a href="#Services">Services</a>
+        </li>
+        <li>
+          <a href="#Portfolio">Portfolio</a>
+        </li>
+        <li>
+          <a href="#Testmonials">Testmonials</a>
+        </li>
+        <li>
+          <a href="#Contact">Contact</a>
+        </li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://linkedin.com/in/">
+          <BsLinkedin />
+        </a>
+        <a href="https://github.com/">
+          <BsGithub />
+        </a>
+        <a href="https://figma.com/in/">
+          <CgFigma />
+        </a>
+      </div>
+      <div className="footer__copyright">
+        <small>&copy; 2022 Leonardo Muniz. All rights reserved.</small>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
