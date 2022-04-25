8152d5a9b44e67135fa541b80d20d41846523320
leomunizq <leonardo.mdq@hotmail.com>
2022-04-25 13:26:53 +0200

    Styletestimonials css, box testimonial

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -8,120 +8,74 @@ import IMG5 from '../../assets/portfolio5.png'
 import IMG6 from '../../assets/portfolio6.jpg'
 
 const Portfolio = () => {
+  const data = [
+    {
+      id: 1,
+      image: IMG1,
+      title: 'this is a portfolio item tittle',
+      github: 'https://github.com/',
+      demo: 'https://github.com/'
+    },
+    {
+      id: 2,
+      image: IMG2,
+      title: 'this is a portfolio item tittle',
+      github: 'https://github.com/',
+      demo: 'https://github.com/'
+    },
+    {
+      id: 3,
+      image: IMG3,
+      title: 'this is a portfolio item tittle',
+      github: 'https://github.com/',
+      demo: 'https://github.com/'
+    },
+    {
+      id: 4,
+      image: IMG4,
+      title: 'this is a portfolio item tittle',
+      github: 'https://github.com/',
+      demo: 'https://github.com/'
+    },
+    {
+      id: 5,
+      image: IMG5,
+      title: 'this is a portfolio item tittle',
+      github: 'https://github.com/',
+      demo: 'https://github.com/'
+    },
+    {
+      id: 6,
+      image: IMG6,
+      title: 'this is a portfolio item tittle',
+      github: 'https://github.com/',
+      demo: 'https://github.com/'
+    }
+  ]
   return (
     <section id="portfolio">
       <h5>My Recent Work</h5>
       <h2>Portfolio</h2>
 
       <div className="container portfolio__container">
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG1} alt="" />
-          </div>
-          <h3>this is a portfolio item tittle</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target="_blank">
-              Github
-            </a>
-            <a
-              href="https://github.com"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG2} alt="" />
-          </div>
-          <h3>this is a portfolio item tittle</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target="_blank">
-              Github
-            </a>
-            <a
-              href="https://github.com"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG3} alt="" />
-          </div>
-          <h3>this is a portfolio item tittle</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target="_blank">
-              Github
-            </a>
-            <a
-              href="https://github.com"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG4} alt="" />
-          </div>
-          <h3>this is a portfolio item tittle</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target="_blank">
-              Github
-            </a>
-            <a
-              href="https://github.com"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG5} alt="" />
-          </div>
-          <h3>this is a portfolio item tittle</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target="_blank">
-              Github
-            </a>
-            <a
-              href="https://github.com"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
-        <article className="portfolio__item">
-          <div className="portfolio__item-image">
-            <img src={IMG6} alt="" />
-          </div>
-          <h3>this is a portfolio item tittle</h3>
-          <div className="portfolio__item-cta">
-            <a href="https://github.com" className="btn" target="_blank">
-              Github
-            </a>
-            <a
-              href="https://github.com"
-              className="btn btn-primary"
-              target="_blank"
-            >
-              Live Demo
-            </a>
-          </div>
-        </article>
+        {data.map(({ id, image, title, github, demo }) => {
+          return (
+            <article key={id} className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={image} alt={title} />
+              </div>
+              <h3>{title}</h3>
+              <div className="portfolio__item-cta">
+                <a href={github} className="btn" target="_blank">
+                  Github
+                </a>
+                <a href={demo} className="btn btn-primary" target="_blank">
+                  Live Demo
+                </a>
+              </div>
+            </article>
+          )
+        })}
       </div>
     </section>
   )
--- src/components/testimonials/Testimonials.tsx
+++ src/components/testimonials/Testimonials.tsx
@@ -1,8 +1,68 @@
 import React from 'react'
 import './testimonials.scss'
+import AVATAR1 from '../../assets/avatar1.jpg'
+import AVATAR2 from '../../assets/avatar2.jpg'
+import AVATAR3 from '../../assets/avatar3.jpg'
+import AVATAR4 from '../../assets/avatar4.jpg'
 
 const Testimonials = () => {
-  return <section id="testimonials">Testimonials</section>
+  return (
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <div className="container testimonials__container">
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVATAR1} alt="Avatar one" />
+          </div>
+          <h5 className="client__name">Giani Monti</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum illo
+            rerum, eligendi laborum dolore quidem. Mollitia enim voluptatibus
+            illo quae vero natus incidunt in, error neque molestias, quis
+            similique dicta?
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVATAR2} alt="Avatar one" />
+          </div>
+          <h5 className="client__name">Giani Monti</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum illo
+            rerum, eligendi laborum dolore quidem. Mollitia enim voluptatibus
+            illo quae vero natus incidunt in, error neque molestias, quis
+            similique dicta?
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVATAR3} alt="Avatar one" />
+          </div>
+          <h5 className="client__name">Giani Monti</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum illo
+            rerum, eligendi laborum dolore quidem. Mollitia enim voluptatibus
+            illo quae vero natus incidunt in, error neque molestias, quis
+            similique dicta?
+          </small>
+        </article>
+        <article className="testimonial">
+          <div className="client__avatar">
+            <img src={AVATAR4} alt="Avatar one" />
+          </div>
+          <h5 className="client__name">Giani Monti</h5>
+          <small className="client__review">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum illo
+            rerum, eligendi laborum dolore quidem. Mollitia enim voluptatibus
+            illo quae vero natus incidunt in, error neque molestias, quis
+            similique dicta?
+          </small>
+        </article>
+      </div>
+    </section>
+  )
 }
 
 export default Testimonials
